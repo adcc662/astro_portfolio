4c7ac566741409686dea9dcf16f8610a29c87975
adcc662 <david@DESKTOP-JH1BAAU>
2024-03-05 17:49:14 -0600

    Adding dependencies and making the first layout

---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = "Personal Portfolio";
+export const SITE_DESCRIPTION = "This template is my personal portfolio";
+export const GENERATE_SLUG_FROM_TITLE = true;
+export const TRANSITION_API = true;
