83ba6f9c3b8de024dd864a3b9639dd9f5322c2c8
adcc662 <david@DESKTOP-JH1BAAU>
2024-03-06 12:09:38 -0600

    Design the components Card and BaseHead

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 import mdx from "@astrojs/mdx";
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx(), sitemap(), tailwind()]
 });
